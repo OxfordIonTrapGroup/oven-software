20cf4218b354bea191907af3e1eb565061de3218
T Ballance <timothy.ballance@physics.ox.ac.uk>
2017-09-25 10:57:13 +0000

    Added temperature feedback measurement scripts

---
+++ software/oven_test_scripts/setup_temperature_feedback/measure_temperature_feedback_risetime.py
@@ -0,0 +1,79 @@
+
+import time
+import numpy as np
+import pickle
+import atomic_oven_controller
+
+channel = int(input("Oven channel to set current controller settings of (0,1): "))
+if channel < 0 or channel > 1:
+    raise Exception("Bad channel: {}".format(channel))
+
+
+p = atomic_oven_controller.OvenPICInterface(timeout=2)
+current_controller = "current_{}".format(channel)
+temperature_controller = "temperature_{}".format(channel)
+
+t_start = 1
+t_on = 10
+t_off = 5
+
+# Temperature to regulate to
+test_setpoint = 100
+
+try:
+
+    current_limits = p.fb_get_limits(current_controller)
+
+    p.fb_set_setpoint(current_controller, 0)
+    p.fb_start(current_controller)
+
+
+    temperature_limits = {}
+    temperature_limits['cv_min'] = 0
+    temperature_limits['cv_max'] = np.sqrt(current_limits['value_max'])
+    temperature_limits['value_max'] = 420
+    temperature_limits['setpoint_slewrate'] = 400 # C/s
+    p.fb_set_limits(temperature_controller, temperature_limits)
+
+    temperature_config = {}
+    temperature_config['p'] = 0.08
+    temperature_config['i'] = 0.0008
+    temperature_config['d'] = -0.1
+    temperature_config['sample_decimation'] = 9 # 100 Hz update rate
+
+    p.fb_set_config(temperature_controller, temperature_config)
+    p.fb_set_setpoint(temperature_controller, test_setpoint)
+
+    # Set up the ADC decimation to reduce data rate by factor of 10
+    p.adc_decimate(9)
+
+    print(p.fb_read_status(temperature_controller))
+    print(p.fb_read_status(current_controller))
+
+    p.adc_start_streaming(list(range(0,8)))
+    time.sleep(t_start)
+    p.fb_start(temperature_controller)
+    time.sleep(t_on/2)
+    print(p.fb_read_status(temperature_controller))
+    time.sleep(t_on/2)
+
+    print(p.fb_read_status(temperature_controller))
+    print(p.fb_read_status(current_controller))
+
+    p.fb_set_setpoint(temperature_controller, 0)
+    time.sleep(t_off)
+
+    p.fb_stop(temperature_controller)
+    print(p.fb_read_status(current_controller))
+
+    results = p.adc_stop_streaming()[channel]
+
+    f = open("temperature_feedback_risetime_data_{}.pkl".format(channel), "wb")
+    pickle.dump(results, f, -1)
+
+
+finally:
+    p.fb_set_setpoint(temperature_controller, 0)
+    p.fb_set_setpoint(current_controller, 0)
+    p.set_pwm_duty(channel, 0)
+
---
+++ software/oven_test_scripts/setup_temperature_feedback/plot_temperature_feedback_risetime.py
@@ -0,0 +1,53 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+
+def set_grid(ax):
+    major_ticks = ax.get_xticks(minor=False)
+    minor_ticks = np.arange(major_ticks[0],
+        major_ticks[-1], (major_ticks[1] - major_ticks[0])/2)
+    ax.set_xticks(minor_ticks, minor=True)
+
+    major_ticks = ax.get_yticks(minor=False)
+    minor_ticks = np.arange(major_ticks[0],
+        major_ticks[-1], (major_ticks[1] - major_ticks[0])/2)
+    ax.set_yticks(minor_ticks, minor=True)
+
+    ax.grid(c='grey', linestyle='--', which='major', alpha=0.5)
+    ax.grid(c='grey', linestyle='--', which='minor', alpha=0.2)
+    ax.set_axisbelow(True)
+
+
+for channel in range(2):
+    try:
+        f = open("temperature_feedback_risetime_data_{}.pkl".format(channel), "rb")
+    except Exception as e:
+        print("Skipping channel {}".format(channel))
+        continue
+
+    results = pickle.load(f)
+
+    fig, ax = plt.subplots(nrows=3, sharex=True)
+    t = np.arange(len(results["T"]))/100
+
+    ax[0].plot(t, results["T"], '.')
+    ax[0].set_ylabel("Temperature (C)")
+
+
+    ax[1].plot(t, results["I"], '.')
+    ax[1].set_ylabel("Current (A)")
+
+    ax[2].plot(t, results["V_out"], '.')
+    ax[2].set_ylabel("Voltage (V)")
+
+    ax[2].set_xlabel("Time (s)")
+
+
+    set_grid(ax[0])
+    set_grid(ax[1])
+    set_grid(ax[2])
+
+
+    plt.savefig("temperature_feedback_risetime_data_{}.pdf".format(channel))
