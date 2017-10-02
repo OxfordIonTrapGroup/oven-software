9712b9caa0044f307171a90736daa0cde0fb55f8
Tim Ballance <timothy.ballance@physics.ox.ac.uk>
2017-10-02 16:06:50 +0100

    Added on-time checking

--- OvenPIC.X/pwm.h
+++ OvenPIC.X/pwm.h
@@ -8,6 +8,8 @@ extern void pwm_set_duty(uint32_t channel, float duty);
 extern void pwm_enable(uint32_t channel);
 extern void pwm_disable(uint32_t channel);
 
+extern uint16_t pwm_duty[2];
+
 #define OVEN_PWM_PERIOD 499
 #define OVEN_MAX_DUTY 0.4
 #define PWM_DECIMATION_BITS 4
