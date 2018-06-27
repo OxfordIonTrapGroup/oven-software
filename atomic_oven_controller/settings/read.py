e6e804db3a1ea844c277a2c0fa95c0bd2795fa52
root <root@ALWSDAQ33.nat.physics.ox.ac.uk>
2018-06-27 10:45:37 +0100

    make file argument optional for read settings

--- atomic_oven_controller/settings/read.py
+++ atomic_oven_controller/settings/read.py
@@ -6,12 +6,16 @@ import argparse
 import artiq.protocols.pyon as pyon
 
 parser = argparse.ArgumentParser(
-    description="Read all of the user adjustable settings to file")
-parser.add_argument("file")
+    description="Read all of the user adjustable settings (file optional)")
+parser.add_argument("-f", "--file")
 args = parser.parse_args()
 
 p = atomic_oven_controller.interface.Interface(timeout=2)
 
 settings = p.settings_read()
 
-pyon.store_file(args.file, settings)
+if args.file is not None:
+    pyon.store_file(args.file, settings)
+else:
+    print(pyon.encode(settings, True))
+
